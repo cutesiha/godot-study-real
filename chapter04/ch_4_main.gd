b3deab86694f81c6affd1f08987591d121e5512e
cutesiha <sihanim7@gmail.com>
2026-06-09 09:06:45 +0900

    ch4_라이프사이클_코인수집게임

--- chapter04/ch_4_main.gd
+++ chapter04/ch_4_main.gd
@@ -1,4 +1,3 @@
-# res://Chapter04/ch_4_main.gd
 extends Node2D
 
 # 자주 쓸 노드는 미리 변수에 담아둔다 (캐싱)
@@ -9,19 +8,24 @@ extends Node2D
 var score := 0
 
 func _ready() -> void:
-	# 코인의 body_entered 시그널을 핸들러에 연결
-	coin.body_entered.connect(_on_coin_body_entered)
 	score_label.text = "Score: 0"
+	print("main_ready 호출됨!!")
+	coin.body_entered.connect(_on_coin_body_entered)
+	coin.body_exited.connect(_on_coin_body_exited)
 
-# 시그널 핸들러: 코인에 무언가 들어오면 자동 호출된다
 func _on_coin_body_entered(body: Node2D) -> void:
-	# 들어온 것이 플레이어일 때만 점수 처리
 	if body == player:
 		score += 1
 		score_label.text = "Score: %d" % score
-		coin.queue_free()  # 코인 제거 → coin의 _exit_tree 호출됨
+		coin.queue_free()
 
 func _exit_tree() -> void:
-	# 게임 종료 시 시그널 해제 (메모리 누수 방지)
 	if is_instance_valid(coin) and coin.body_entered.is_connected(_on_coin_body_entered):
 		coin.body_entered.disconnect(_on_coin_body_entered)
+
+	if is_instance_valid(coin) and coin.body_exited.is_connected(_on_coin_body_exited):
+		coin.body_exited.disconnect(_on_coin_body_exited)
+
+func _on_coin_body_exited(body: Node2D) -> void:
+	# print("body_exited 이벤트 발생!!")
+	pass
